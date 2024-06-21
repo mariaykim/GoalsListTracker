9715ecb1572a6f08911ab656106f40148a17a1fa
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-06-21 15:53:22 -0400

    initial commit

---
+++ GoalsListTracker/GoalsListTracker/GoalsListTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsListTrackerApp.swift
+//  GoalsListTracker
+//
+//  Created by Maria Kim on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GoalsListTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
