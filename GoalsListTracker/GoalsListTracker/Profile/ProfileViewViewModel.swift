43bf5fb099e66ef25343b651d0cfebd9d178a04b
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-06-25 10:33:50 -0400

    Update ProfileViewViewModel.swift

--- GoalsListTracker/GoalsListTracker/Profile/ProfileViewViewModel.swift
+++ GoalsListTracker/GoalsListTracker/Profile/ProfileViewViewModel.swift
@@ -14,9 +14,7 @@ final class ProfileViewViewModel: ObservableObject {
     
     func loadCurrentUser() async throws {
         let authDataResult = try AuthenticationManager.shared.getAuthenticatedUser()
-        print("authdata \(authDataResult)")
         self.user = try await UserManager.shared.getUser(userId: authDataResult.uid)
-        print("\(user)")
     }
     
     func toggleUserPremiumStatus() {
